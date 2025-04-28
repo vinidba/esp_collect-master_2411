95cc6ab7f94b700fa1ef03d7ea5c53cce7f45c59
viniciusdba <41973556+vinidba@users.noreply.github.com>
2025-04-28 15:02:39 -0500

    Add files via upload

---
+++ run_esp_master_solaris.sh
@@ -0,0 +1,29 @@
+# eSP collector for Solaris (2016-09-01)
+echo "Start eSP collector."
+
+export ORAENV_ASK=NO
+ 
+ORATAB=/var/opt/oracle/oratab
+ 
+db=`egrep -i ":Y|:N" $ORATAB | cut -d":" -f1 | grep -v "\#" | grep -v "\*"`
+for i in $db ; do
+       export ORACLE_SID=$i
+       . oraenv
+ 
+
+sqlplus -s /nolog <<EOF
+connect / as sysdba
+
+@sql/esp_master.sql
+EOF
+ 
+done
+
+zip -qm esp_recycle_bin.zip cpuinfo_model_name_*.txt 
+zip -qm esp_recycle_bin.zip escp_*_*.csv 
+zip -qm esp_recycle_bin.zip esp_requirements_*_*_*.csv 
+zip -qm esp_recycle_bin.zip res_requirements_*_*_*.txt 
+zip -qm esp_recycle_bin.zip features_use_*_*_*.txt 
+rm esp_recycle_bin.zip
+
+echo "End eSP collector. Output: escp_output_hostname_yyyymmdd.zip"
